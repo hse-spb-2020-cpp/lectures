e08a92b190cb682be26c8d59c7511a6dc1098cb2
Egor Suvorov <egor.suvorov@gmail.com>
2021-03-02 12:59:15 +0300

    17-210209/02-cross/02.cpp: add hex dump of virtual tables

--- 17-210209/02-cross/02.cpp
+++ 17-210209/02-cross/02.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <iomanip>
 #include <cstddef>
 // https://isocpp.org/wiki/faq/multiple-inheritance#mi-delegate-to-sister
 
@@ -25,20 +26,39 @@ int main() {
     DerivedX a;
     a.foo();
 
-    std::cout << sizeof(Base) << "\n";
-    std::cout << sizeof(Derived1) << "\n";
-    std::cout << sizeof(Derived2) << "\n";
-    std::cout << sizeof(DerivedX) << "\n";
+    // Комментарии дальше специфичны для машины Егора.
+    // У вас могут быть абсолютно другие результаты.
 
-    std::cout << &static_cast<Base&>(a) << "\n";
-    std::cout << &static_cast<Derived1&>(a) << "\n";
-    std::cout << &static_cast<Derived2&>(a) << "\n";
-    std::cout << &a << "\n";
+    std::cout << sizeof(Base) << "\n";  // 16: vptr (8), int (4), padding (4)
+    std::cout << sizeof(Derived1) << "\n";  // 8+16: ptr-to-Base (8) | Base (16)
+    std::cout << sizeof(Derived2) << "\n";  // 8+16: ptr-to-Base (8) | Base (16)
+    std::cout << sizeof(DerivedX) << "\n";  // 16+16: Derived1 (8),  Derived2 (8) | Base (16)
 
-    // TODO: что тут выводится?
+    std::cout << &static_cast<Base&>(a) << "\n";  // X+0x10
+    std::cout << &static_cast<Derived1&>(a) << "\n";  // X+0x00
+    std::cout << &static_cast<Derived2&>(a) << "\n";  // X+0x08
+    std::cout << &a << "\n";  // X+0x00
+
+    /*
+    У Егора (msys2 mingw-w64-x86_64) выводится:
+    0xf50f9c0
+    0xf50f9b0
+    0xf50f9b8
+    0xf50f9b0
+    // TODO: к сожалению, непонятно, что это за указатели. Возможно, относительные?
+    d8 46 e7 3f // Derived 1
+    01 00 00 00
+    f8 46 e7 3f // Derived 2
+    01 00 00 00
+    20 47 e7 3f // Base
+    01 00 00 00
+    64 00 00 00 // Base::field
+    00 00 00 00
+    */
+    std::cout << std::hex;
     for (std::size_t i = 0; i < sizeof(DerivedX); i++) {
         char *ptr = reinterpret_cast<char*>(&a) + i;
-        std::cout << (static_cast<int>(*ptr) & 0xFF) << " ";
+        std::cout << std::setfill('0') << std::setw(2) << (static_cast<int>(*ptr) & 0xFF) << " ";
         if (i % 4 == 3) {
             std::cout << "\n";
         }
